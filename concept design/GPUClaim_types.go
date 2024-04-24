297e4e2d3c3a60de0c6c80f34539c8265d83b598
lzhchn0 <136823672+lzhchn0@users.noreply.github.com>
2024-04-24 16:07:47 -0400

    Create GPUClaim_types.go
    Signed-off-by: lzhchn0 <136823672+lzhchn0@users.noreply.github.com>

---
+++ concept design/GPUClaim_types.go
@@ -0,0 +1,69 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// GPUClaimSpec defines the desired state of GPUClaim
+type GPUClaimSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of GPUClaim. Edit gpuclaim_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+
+	// Replicas is the number of desired pods. Defaults to the default deployment
+	// replicas value.
+	// +optional
+	Replicas *int32 `json:"replicas,omitempty"`
+}
+
+// GPUClaimStatus defines the observed state of GPUClaim
+type GPUClaimStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// GPUClaim is the Schema for the gpuclaims API
+type GPUClaim struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   GPUClaimSpec   `json:"spec,omitempty"`
+	Status GPUClaimStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// GPUClaimList contains a list of GPUClaim
+type GPUClaimList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []GPUClaim `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&GPUClaim{}, &GPUClaimList{})
+}
